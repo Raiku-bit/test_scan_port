e9dd3c615b61bfa6ad76920f1c2a080d8799c56b
Raiku-bit <estudio2107@gmail.com>
2026-01-13 17:08:43 -0400

    [FEATURE] Added recognition of protocol, port, IP, and connection status.

--- scan_port.py
+++ scan_port.py
@@ -7,11 +7,21 @@ def tranformar_mb(bytes_value):
 while True:
     #obtain global network statistics
     netword = psutil.net_io_counters()
-    
     send_mb = tranformar_mb(netword.bytes_sent)
     received_mb = tranformar_mb(netword.bytes_recv)
-    #Display data
+    #Display Data
     print(f'Total Enviado: {send_mb} MB | Total Recibido: {received_mb} MB', end='\r')
-    time.sleep(1)
+    
+    #conexion activated
+    networkd_connections = psutil.net_connections()
+    for connection in networkd_connections[:15]:
+        laddr = f'{connection.laddr.ip}:{connection.laddr.port}'
+        raddr = f'{connection.raddr.ip}:{connection.raddr.port}' if connection.raddr else "N/A"
+        types = 'TCP' if connection.type == 1 else 'UDP'
+        #Display Data       
+        print('--//----Protocol------------IP--------------PORT----------Status Connection------//')
+        print(f'//    {types:<6}    {laddr:<25}       {raddr:<25}      {connection.status}     //')   
+        print('//-----------------------------------------------------------------------------//')
+    time.sleep(5)
 
     
